792175f10c2ebfc96dc162c35c81eedafff1cd9b
Xavier <xavier.tonnellier@epitech.eu>
2023-02-10 16:43:03 +0100

    feat(main): init repo with 3d example

---
+++ SuperHeroes.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+if [ "$#" -eq 1 ] && [ "$1" = "debug" ]; then
+    echo "-- DEBUG MODE"
+    cmake -S . -B build -DCMAKE_BUILD_TYPE=debug
+else
+    cmake -S . -B build -DCMAKE_BUILD_TYPE=release
+fi
+cmake --build build -j 8
