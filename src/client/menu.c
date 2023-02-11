847722b0fd260e5a387808f2a69cbd95447c5791
Jean-Yanis <jean-yanis.jeffroy@epitech.eu>
2023-02-11 17:48:01 +0100

    feat(Menu) quit button

--- src/client/menu.c
+++ src/client/menu.c
@@ -6,15 +6,34 @@ static void menu_ui(menu_t *menu_st)
     BeginDrawing();
 
     menu_st->btnStartAction = false;
-    menu_st->sourceRec = (Rectangle){ 0, 0, (float)menu_st->button.width / 2, menu_st->button.height};
     DrawTextureRec(menu_st->button, menu_st->sourceRec, (Vector2){((GetMonitorWidth(GetCurrentMonitor()) / 2) - (menu_st->button.width / 3)),
     (GetMonitorHeight(GetCurrentMonitor()) - menu_st->button.height) - 50}, WHITE);
 
     if (CheckCollisionPointRec(menu_st->mousePoint, menu_st->btnBounds)) {
+        menu_st->sourceRec.x = menu_st->button.width / 2;
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) menu_st->btnStartState = 2;
         else menu_st->btnStartState = 1;
+    } else {
+        menu_st->sourceRec.x = 0;
+        menu_st->btnStartState = 0;
     }
-    else menu_st->btnStartState = 0;
+
+    menu_st->quit_btnStartAction = false;
+    DrawTextureRec(menu_st->quit_button, menu_st->quit_sourceRec, (Vector2){menu_st->quit_position.x, menu_st->quit_position.y}, WHITE);
+
+    if (CheckCollisionPointRec(menu_st->mousePoint, menu_st->quit_btnBounds)) {
+        menu_st->quit_sourceRec.x = menu_st->quit_button.width / 2;
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            menu_st->quit_position.x = rand() % (GetMonitorWidth(GetCurrentMonitor()) - menu_st->quit_button.width / 2);
+            menu_st->quit_position.y = rand() % (GetMonitorHeight(GetCurrentMonitor()) - menu_st->quit_button.height);
+            menu_st->quit_btnBounds = (Rectangle){menu_st->quit_position.x, menu_st->quit_position.y, menu_st->quit_button.width / 2, menu_st->quit_button.height};
+            menu_st->quit_btnStartState = 2;
+        }
+        else menu_st->quit_btnStartState = 1;
+    } else {
+        menu_st->quit_sourceRec.x = 0;
+        menu_st->quit_btnStartState = 0;
+    }
 
     DrawTextureEx(menu_st->title, (Vector2){(GetMonitorWidth(GetCurrentMonitor()) - menu_st->title.width) / 2, 50}, 0.0f, 1.0f, WHITE);
 
